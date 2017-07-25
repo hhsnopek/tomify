d7698739d866971b23cf8a642515fff8eebc5f5a
Henry Snopek <hhsnopek@gmail.com>
2017-07-25 18:10:48 -0400

    improve comments, proper centering of tom.gif

--- index.js
+++ index.js
@@ -14,14 +14,14 @@ const cli = meow(
 
   Options:
     --output, -o Output Path
+    --resize, -r Resize Number, 1.25
     --debug, -d Debug
 
   Examples:
     $ tomify file1.jpg file2.jpg -o ./new-folder
 `,
   {
-    alias: { h: 'help', o: 'output', d: debug },
-    defaults: { debug: false }
+    alias: { h: 'help', o: 'output', d: 'debug', r: 'resize' }
   }
 )
 
@@ -33,6 +33,11 @@ if (cli.input.length === 0 && !cli.flags.input) {
   console.error('Specify at least one path')
   process.exit(1)
 }
+if (!cli.flags.resize) cli.flags.resize = 1
+else if (typeof cli.flags.resize !== 'number') {
+  console.error('resize must be a number')
+  process.exit(1)
+}
 
 let dest = cli.flags.output || process.cwd()
 fs.ensureDirSync(dest)
@@ -41,6 +46,7 @@ console.error('Destination: %s', dest)
 /**
  * Build Toms
  */
+
 const toms = []
 for (let file of cli.input) {
   toms.push(
@@ -58,6 +64,10 @@ Promise.all(toms)
     process.exit(err.code || 1)
   })
 
+/**
+ * Find all Faces
+ */
+
 function findFace(file, dest) {
   return new Promise((resolve, reject) => {
     debug('Processing %s...', file)
@@ -69,14 +79,20 @@ function findFace(file, dest) {
 
       debug('Face face in %s...', file)
       const face = faces[0]
-      const height = parseInt(face.getHeight() * 1.75)
-      const width = parseInt(face.getWidth() * 1.75)
-      const top = parseInt(face.getX() - (height * .25))
-      const left = parseInt(face.getY() - (width * .25))
-      resolve([dest, file, top, left, height, width])
+      const centerX = (face.getX() + (face.getWidth() / 2))
+      const centerY = (face.getY() + (face.getHeight() / 2))
+      const tomWidth = cli.flags.resize * (face.getWidth() * 1.75)
+      const tomHeight = cli.flags.resize * (face.getHeight() * 1.75)
+      const x = centerX - (tomWidth / 2)
+      const y = centerY - (tomHeight / 2)
+      resolve([dest, file, x, y, tomHeight, tomWidth])
     })
   })
 }
+
+/**
+ * Add Tom to Image
+ */
 
 function addTom([dest, file, top, left, height, width]) {
   let meta = path.parse(file)
@@ -107,6 +123,10 @@ function addTom([dest, file, top, left, height, width]) {
     }
   )
 }
+
+/**
+ * Debug
+ */
 
 function debug(msg) {
   if (cli.flags.debug) console.error(msg)
